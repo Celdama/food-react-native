b03f87c15608d3545b983d862f44b9663a7f1579
Celdama <celdama.dev@gmail.com>
2022-08-11 13:31:50 +0200

    App: finish initial navigator setup

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import SearchScreen from './src/screens/SearchScreen';
 
-const navigator = createStackNavigator({
-  Search: SearchScreen,
-});
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    initialRouteName: 'Search',
+    defaullNavigationOptions: {
+      title: 'Business Search',
+    },
+  }
+);
