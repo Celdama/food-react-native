5569a1d90f4b6ab201138fbdc6aca802896f23e5
Celdama <celdama.dev@gmail.com>
2022-08-11 13:32:21 +0200

    App: import

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import SearchScreen from './src/screens/SearchScreen';
@@ -13,3 +14,5 @@ const navigator = createStackNavigator(
     },
   }
 );
+
+export default createAppContainer(navigator);
