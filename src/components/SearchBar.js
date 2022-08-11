f1efe808d1d64b951e4ddc84b25011148dad323d
Celdama <celdama.dev@gmail.com>
2022-08-11 13:37:58 +0200

    components/SearchBar: create

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search bar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
