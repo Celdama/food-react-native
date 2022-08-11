06b67599b31c3e567f64a3d5b24f35525d0cfbb6
Celdama <celdama.dev@gmail.com>
2022-08-11 13:27:00 +0200

    screens/SearchScreen: create

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
