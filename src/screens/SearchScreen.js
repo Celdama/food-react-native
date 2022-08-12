c201dbeb7822d07ada7c49f51e4f834fb7527279
Celdama <celdama.dev@gmail.com>
2022-08-12 09:24:46 +0200

    screens/SearchScreen: setUp searchApi function using async await

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'san jose',
+      },
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log('submitted')}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       <Text>Search screen</Text>
+      <Text>we have found {results.length} results</Text>
     </View>
   );
 };
