41fc1efac664f7ae65b359725c30dea33f91e3b8
Celdama <celdama.dev@gmail.com>
2022-08-12 09:53:00 +0200

    src: using custom hook done

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose',
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  useEffect(() => {
-    searchApi('Pasta');
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
