3508ccd2a533b6ed5fe015e635414a2d9157115f
Celdama <celdama.dev@gmail.com>
2022-08-13 11:17:32 +0200

    screens/ResultsShowScreen: fetch Restaurant details

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import yelp from '../api/yelp';
+
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
-  console.log(id);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
   return (
     <View>
       <Text>ResultsShowScreen</Text>
