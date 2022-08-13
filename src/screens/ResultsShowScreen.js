6467775a812ad575a70c3e12475476d241e2f51e
Celdama <celdama.dev@gmail.com>
2022-08-13 10:08:54 +0200

    create ResultsShowScreen and added it on navigator

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ResultsShowScreen = () => {
+  return (
+    <View>
+      <Text>ResultsShowScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
