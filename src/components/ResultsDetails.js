184c6761f9332a03af52a7c0787dab79a89c2ba0
Celdama <celdama.dev@gmail.com>
2022-08-13 08:52:42 +0200

    components/ResultsDetails: create

---
+++ src/components/ResultsDetails.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native-web';
+
+const ResultsDetails = () => {
+  return (
+    <View>
+      <Text>Results Details</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsDetails;
