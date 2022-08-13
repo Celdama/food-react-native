9f17ec92ce56335076402b5d1e6b92b235231087
Celdama <celdama.dev@gmail.com>
2022-08-13 11:10:59 +0200

    src: get id from navigator using a second props to be able to fetch restaurant one by one

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const id = navigation.getParam('id');
+  console.log(id);
   return (
     <View>
       <Text>ResultsShowScreen</Text>
