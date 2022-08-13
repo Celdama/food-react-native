2e6658f3604e6844fccabb29a166e6716ff7cdf6
Celdama <celdama.dev@gmail.com>
2022-08-13 08:55:59 +0200

    rename ResultsDetails component into ResultsDetail

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ResultsDetail = () => {
+  return (
+    <View>
+      <Text>Results Details</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsDetail;
