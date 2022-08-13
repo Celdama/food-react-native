41aa65c0066d2dfc3169464ee577b71026ad8afe
Celdama <celdama.dev@gmail.com>
2022-08-13 09:04:26 +0200

    components/Results: add somme styling to ResultsDetail component

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,13 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-const ResultsDetail = () => {
+const ResultsDetail = ({ result }) => {
+  const { image_url, name, rating, review_count } = result;
   return (
     <View>
-      <Text>Results Details</Text>
+      <Image style={styles.imgStyle} source={{ uri: image_url }} />
+      <Text style={styles.nameStyle}>{name}</Text>
+      <Text>
+        {rating} Stars, {review_count} Reviews
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imgStyle: {
+    height: 130,
+    width: 250,
+    borderRadius: 4,
+  },
+  nameStyle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
 
 export default ResultsDetail;
